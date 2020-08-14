1cb37357faea72be4e62b4d43f74537d43c42edf
lb5848 <lbiavat1@jhmi.edu>
2020-08-14 17:58:35 -0400

    minor bug in flowAI function

--- FlowCyWorkflow_part1_createSCE.R
+++ FlowCyWorkflow_part1_createSCE.R
@@ -132,7 +132,7 @@ if(!dir.exists(QC_dir)){
 
 fs <- flowCore::fsApply(flowSet, function(ff){
   resQC_AI <- flow_auto_qc(fcsfiles = ff,
-                        folder_results = file.path(QC_dir, paste("flowAI", ff@description$GUID, sep = "/")),
+                        folder_results = file.path(QC_dir, paste("flowAI", gsub(".fcs", " Folder", ff@description$GUID), sep = "/")),
                         output = 1)
   resQC <- PeacoQC(ff = ff,
                    determine_good_cells = "all",
